f5b96a5a16cdcc80b37f1c0c78816372f22fb238
Андрей <mazurenkoam.93@yandex.ru>
2025-06-17 10:38:50 +0300

    Разделить логику работы сервиса и хранилища для User

--- src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
+++ src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
@@ -1,12 +1,18 @@
 package ru.practicum.shareit.user.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import ru.practicum.shareit.exception.InternalServerException;
+import ru.practicum.shareit.exception.NotFoundException;
 import ru.practicum.shareit.user.User;
 import ru.practicum.shareit.user.storage.UserStorage;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
@@ -20,16 +26,41 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User findUserById(Long id) {
-        return userStorage.findUserById(id);
+        User user = userStorage.findUserById(id);
+        if (user == null) {
+            log.warn("Пользователь с данным id {} не найден", id);
+            throw new NotFoundException("Пользователь с данным id: " + id + " не найден");
+        }
+        return user;
     }
 
     @Override
     public User createUser(User user) {
+        if (userStorage.findAllUsers().stream()
+                .anyMatch(viewedUser -> Objects.equals(viewedUser.getEmail(), user.getEmail()))) {
+            log.warn("Данная электронная почта {} уже существует", user.getEmail());
+            throw new InternalServerException("Данная электронная почта уже существует");
+        }
+
         return userStorage.createUser(user);
     }
 
     @Override
     public User updateUser(User updateUser) {
+        Optional<User> existingUserWithEmail = userStorage.findAllUsers().stream()
+                .filter(viewedUser -> Objects.equals(viewedUser.getEmail(), updateUser.getEmail()) && !updateUser.getId().equals(viewedUser.getId()))
+                .findAny();
+        if (existingUserWithEmail.isPresent()) {
+            log.warn("Данная электронная почта {} уже существует, и запрашиваемый id {} не найден", updateUser.getEmail(), updateUser.getId());
+            throw new InternalServerException("Данная электронная почта уже существует и запрашиваемый id не найден");
+        }
+
+        User existingUser = userStorage.findUserById(updateUser.getId());
+        if (existingUser == null) {
+            log.warn("Пользователь с id: {} не найден", updateUser.getId());
+            throw new NotFoundException("Пользователь с данным id " + updateUser.getId() + " не найден.");
+        }
+
         return userStorage.updateUser(updateUser);
     }
 
--- src/main/java/ru/practicum/shareit/user/storage/UserStorageImpl.java
+++ src/main/java/ru/practicum/shareit/user/storage/UserStorageImpl.java
@@ -2,8 +2,6 @@ package ru.practicum.shareit.user.storage;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
-import ru.practicum.shareit.exception.InternalServerException;
-import ru.practicum.shareit.exception.NotFoundException;
 import ru.practicum.shareit.user.User;
 
 import java.util.*;
@@ -20,22 +18,11 @@ public class UserStorageImpl implements UserStorage {
 
     @Override
     public User findUserById(Long id) {
-        User user = users.get(id);
-        if (user == null) {
-            log.warn("Пользователь с данным id {} не найден", id);
-            throw new NotFoundException("Пользователь с данным id: " + id + " не найден");
-        }
-        return user;
+        return users.get(id);
     }
 
     @Override
     public User createUser(User user) {
-        if (users.values().stream()
-                .anyMatch(viewedUser -> Objects.equals(viewedUser.getEmail(), user.getEmail()))) {
-            log.warn("Данная электронная почта {} уже существует", user.getEmail());
-            throw new InternalServerException("Данная электронная почта уже существует");
-        }
-
         user.setId(getId());
         users.put(user.getId(), user);
         return user;
@@ -43,19 +30,6 @@ public class UserStorageImpl implements UserStorage {
 
     @Override
     public User updateUser(User updateUser) {
-        if (users.values().stream()
-                .anyMatch(viewedUser -> Objects.equals(viewedUser.getEmail(), updateUser.getEmail()) && !updateUser.getId().equals(viewedUser.getId()))) {
-            log.warn("Данная электронная почта {} уже существует, и запрашиваемый id {} не найден", updateUser.getEmail(), updateUser.getId());
-            throw new InternalServerException("Данная электронная почта уже существует и запрашиваемый id не найден");
-        }
-
-        User existingUser = users.get(updateUser.getId());
-        if (existingUser == null) {
-            log.warn("Пользователь с id: {} не найден", updateUser.getId());
-            throw new NotFoundException("Пользователь с данным id " + updateUser.getId() + " не найден.");
-        }
-
-        users.remove(existingUser.getId());
         users.put(updateUser.getId(), updateUser);
         return updateUser;
     }
