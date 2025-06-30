0769d653c167a45595662ea6bdf575f5da781bea
Андрей <mazurenkoam.93@yandex.ru>
2025-06-30 20:10:47 +0300

    Создать базу данных для сущностей

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -1,22 +1,33 @@
 package ru.practicum.shareit.booking;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
 
 import java.time.LocalDateTime;
 
-/**
- * TODO Sprint add-bookings.
- */
+@NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Entity
+@Table(name = "bookings")
 public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private LocalDateTime bookingStartDate;
-    private LocalDateTime endDateOfBooking;
+    @Column(name = "start_date")
+    private LocalDateTime start;
+    @Column(name = "end_date")
+    private LocalDateTime end;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id", nullable = false)
     private Item item;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "booker_id", nullable = false)
     private User booker;
-    private RentalStatus rentalStatus;
+    @Enumerated(EnumType.STRING)
+    private RentalStatus status;
 }
